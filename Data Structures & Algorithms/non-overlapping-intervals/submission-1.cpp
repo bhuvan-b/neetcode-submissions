81a69f6bc0f6b991abb6a663f049c58f1e6f1abe
Bhuvan Bokka <73696042+bhuvan-b@users.noreply.github.com>
2026-07-11 21:19:05 +0530

    Add: non-overlapping-intervals - submission-1

---
+++ Data Structures & Algorithms/non-overlapping-intervals/submission-1.cpp
@@ -0,0 +1,53 @@
+class Solution {
+public:
+    int eraseOverlapIntervals(vector<vector<int>>& intervals) {
+        sort(intervals.begin(), intervals.end());
+        deque<vector<int>> q;
+        vector<vector<int>> ans;
+        int cnt = 0;
+
+        for(int i=0;i<intervals.size();i++)
+        {
+            q.push_back(intervals[i]);
+        }
+
+        while(!q.empty())
+        {
+            vector<int> el = q.front();
+            q.pop_front();
+            
+            if(q.empty())
+            {
+                ans.push_back(el);
+                break;
+            }
+
+            vector<int> fr = q.front();
+
+            if(fr[0]<el[1])
+            {
+                q.pop_front();
+                cnt++;
+                if(el[1]<fr[1])
+                {
+                    q.push_front(el);
+                }
+                else
+                {
+                    q.push_front(fr);
+                }
+            }
+            else
+            {
+                ans.push_back(el);
+            }
+        }
+
+        return cnt;
+    }
+};
+// 1,11  2,10  11,22
+
+
+// sort & remove the longer interval
+
